8df22da88f396e47b9c13e63fc1bd2e018d8ebb6
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-11 14:34:08 -0800

    retry key removal

--- frontend/utils/youtube.js
+++ frontend/utils/youtube.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCwK9g3u1pDVwsaipVwWQXST_4YY24Q2HY';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
