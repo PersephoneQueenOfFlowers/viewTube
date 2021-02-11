b167c88de7576b4e769f1d0ddfece3c63e8d64e4
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-11 14:26:35 -0800

    remove key

--- frontend/components/nav_bar/welcome_bar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Navbar from './nav_bar';
-
-export default ({ user }) => (
-  <header className="nav-bar">
-    <h4>Welcome { user.username }!</h4>
-  </header>
-);
