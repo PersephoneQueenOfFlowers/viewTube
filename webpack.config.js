d875851781965a80f5aca558185fb9b60b259f62
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-11 17:57:06 -0800

    command zed alot

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,25 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/env', '@babel/react']
+          presets: ['env', 'react']
         }
+      },
+      {
+        test: [/\.(png|jp(e*)g|svg|gif)$/],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*'],
-  }
+  },
+  mode: "development"
 };
-
-
