4e070f77c31c97b7df97fd3280a1b282e6772523
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-11 17:53:10 -0800

    lower case a awesome

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faMusic, faTrophy, faDiceD6, faFilm, faNewspaper } from '@fortAwesome/free-solid-svg-icons';
+import { faHome, faMusic, faTrophy, faDiceD6, faFilm, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 
 class Sidebar extends React.Component {
   constructor(props) {
